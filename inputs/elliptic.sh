48d9425fc6db6ac86188ee875b6103596d547217
Johan de Jong <johan@hornet.(none)>
2008-01-09 21:16:15 -0500

    Repaired the script.
    	modified:   inputs/elliptic.sh

--- inputs/elliptic.sh
+++ inputs/elliptic.sh
@@ -10,20 +10,56 @@ B=$5
 
 echo $P $N $A $B
 
-sed "s@^.*= @@" inputs/input3 > inputs/input
+if [ -f inputs/tijdelijk ]; then
+	echo "Please remove the file inputs/tijdelijk. Stop."
+	exit 1
+fi
+grep -q "^#define LEX_ORDER" data.h
+if [ ! $? = 0 ]; then
+	echo "Make sure you use LEX_ORDER. Stop."
+	exit 1
+fi
+DISC=$((27*$B*$B + 4*$A*$A*$A))
+DISC=$((DISC % $P))
+if [ $DISC = 0 ]; then
+	echo "Discriminant is zero! Stop."
+	exit 1
+fi
+
+cat >> inputs/tijdelijk << AISEJOHAN
+1
+0
+0
+0
+0
+a
+0
+-1
+0
+b
+AISEJOHAN
+
 sed -i -e "s@a@$A@" \
-	-e "s@b@$B@" inputs/input
+	-e "s@b@$B@" inputs/tijdelijk
 
 sed -i \
 	-e "s@^#define d1\t.*@#define d1\t1@" \
 	-e "s@^#define d2\t.*@#define d2\t1@" \
 	-e "s@^#define d3\t.*@#define d3\t1@" \
+	-e "s@^#define d\t.*@#define d\t3@" \
 	-e "s@^#define p\t.*@#define p\t$P@" \
         -e "s@^#define r\t.*@#define r\t$N@" \
         -e "s@^#define q\t.*@#define q\t$Q@" data.h
 
+cat data.h
+echo
+echo "Does this look ok? (Hit return to continue,Ctrl-C to abort.)"
+read ANTWOORD
+
 make input_pol
 
-time ./tester < inputs/input
+time ./tester < inputs/tijdelijk
+
+rm inputs/tijdelijk
 
 exit 0
