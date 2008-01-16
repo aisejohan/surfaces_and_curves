8c906dc2a0d241371ab54fe1ed822a5bdb3073ef
Johan de Jong <johan@paard.apt.columbia.edu>
2008-01-16 17:37:12 -0500

    Make find_new.c work for curves.
    	modified:   find_new.c

--- find_new.c
+++ find_new.c
@@ -22,13 +22,13 @@ void my_alloc(void )
 		lijst = (int **) malloc(100*sizeof(int *));
 		storage = 100;
 		for(i=0; i+1 <= 100;i++) {
-			lijst[i] = (int *) malloc(4*sizeof(int));
+			lijst[i] = (int *) malloc(3*sizeof(int));
 		}
 		return;
 	}
 	lijst = (int **) realloc(lijst, 2*storage*sizeof(int *));
 	for(i=storage; i+1 <= 2*storage;i++) {
-		lijst[i] = (int *) malloc(4*sizeof(int));
+		lijst[i] = (int *) malloc(3*sizeof(int));
 	}
 	storage = 2*storage;
 	return;
@@ -37,34 +37,29 @@ void my_alloc(void )
 
 /* Returns the number of monomials in degree degree, and sets lijst
  * equal to the list of them.*/
-int count_sum(int d1, int d2, int d3, int d4, int degree, int save)
+int count_sum(int d1, int d2, int d3, int degree, int save)
 {
-	int len,a1,a2,a3;
+	int len,a1,a2;
 	
 	len=0;
 	a1 = 0;
 	while (d1*a1 <= degree) {
 		a2 = 0;
 		while (d1*a1+d2*a2 <= degree) {
-			a3 = 0;
-			while (d1*a1+d2*a2+d3*a3 <= degree) {
-				if((degree - (a1*d1+a2*d2+a3*d3)) % d4 == 0) {
-					if (save) {
-						if (len == storage) my_alloc();
-						lijst[len][0] = a1;
-						lijst[len][1] = a2;
-						lijst[len][2] = a3;
-						lijst[len][3] = (degree - 
-						(a1*d1+a2*d2+a3*d3)) / d4;
+			if((degree - (a1*d1+a2*d2)) % d3 == 0) {
+				if (save) {
+					if (len == storage) my_alloc();
+					lijst[len][0] = a1;
+					lijst[len][1] = a2;
+					lijst[len][2] = (degree - 
+						(a1*d1+a2*d2)) / d3;
 					}
 					len++;
-				};
-				a3++;
-			};
+			}
 			a2++;
-		};
+		}
 		a1++;
-	};
+	}
 	return(len);
 }
 
@@ -74,11 +69,10 @@ void print_sum(int len)
 	int i;
 
 	for(i=0;i+1<=len;i++) {
-		printf("[%d, %d, %d, %d]\n",
+		printf("[%d, %d, %d]\n",
 			lijst[i][0],
 			lijst[i][1],
-			lijst[i][2],
-			lijst[i][3]);
+			lijst[i][2]);
 	}
 	return;
 }
@@ -98,20 +92,11 @@ int gcd(int a, int b)
 /* This tests whether three out of four are divisible by the same prime.*
  * If so then the weighted projective space has a codimension 1 locus	*
  * of quotient singularities.						*/
-int well_formed(int d1,  int d2,  int d3,  int d4)
+int well_formed(int d1,  int d2,  int d3)
 {
-	int g;
-
-	g = gcd(d1,d2);
-	if (g > 1) {
-		if (gcd(g,d3) > 1) return(0);
-		if (gcd(g,d4) > 1) return(0);
-	}
-	g = gcd(d3,d4);
-	if (g > 1) {
-		if (gcd(g,d1) > 1) return(0);
-		if (gcd(g,d2) > 1) return(0);
-	}
+	if (gcd(d1,d2) > 1) return(0);
+	if (gcd(d1,d3) > 1) return(0);
+	if (gcd(d2,d3) > 1) return(0);
 	return(1);
 }
 
@@ -121,129 +106,87 @@ int well_formed(int d1,  int d2,  int d3,  int d4)
  * ASSUMES: degree is bigger than d_i for all i. */
 int d_suitable(int len)
 {
-	int i, j, success, sum;
-	int a,b,c,d;
-	int test_one[4];
-	int test_two[4][4];
-	int test_three[4];
+	int i, j, k, success, sum;
+	int test_one[3];
+	int test_two[3];
 
-	for (j=0;j<=3;j++) {
-		test_three[j] = 0;
+	for (j=0;j<=2;j++) {
+		test_two[j] = 0;
 		test_one[j] = 0;
-		for (i=j+1;i<=3;i++) test_two[j][i]=0;
 	}	
 
 	for (i=0;i+1<=len;i++) {
-		sum = lijst[i][0] + lijst[i][1] + lijst[i][2] + lijst[i][3];
-		for(j=0;j<=3;j++) {
-			if (lijst[i][j] == 0) test_three[j] = 1;
+		sum = lijst[i][0] + lijst[i][1] + lijst[i][2];
+		for(j=0;j<=2;j++) {
+			if (lijst[i][j] == 0) {
+				test_two[j] = 1;
+				k=0;
+				while (k <= 2) {
+					if ((k != j) && lijst[i][j] == 1)
+						test_one[3-j-k] = 1;
+					k++;
+				}
+			}
 			if (sum == lijst[i][j]) test_one[j] = 1;
-		}
-		for(j=0;j<=5;j++) {
-			a=pairs[j][0];
-			b=pairs[j][1];
-			c=pairs[j][2];
-			d=pairs[j][3];
-			if ((lijst[i][c] == 0) && (lijst[i][d] == 0)) {
-				test_two[a][b] = 1;
-				if (lijst[i][a] == 1) test_one[b] = 1;
-				if (lijst[i][b] == 1) test_one[a] = 1;
-			}
-			if ((lijst[i][c] == 1) && (lijst[i][d] == 0)) {
-				if (test_two[a][b] != 1) {
-					if (test_two[a][b] == -d-1) {
-						test_two[a][b] = 1;
-					} else {
-						test_two[a][b] = -c-1;
-					}
-				}
-			}
-			if ((lijst[i][c] == 0) && (lijst[i][d] == 1)) {
-				if (test_two[a][b] != 1) {
-					if (test_two[a][b] == -c-1) {
-						test_two[a][b] = 1;
-					} else {
-						test_two[a][b] = -d-1;
-					}
-				}
-			}
 		}
 	}
 	success=0;
-	for (j=0;j<=3;j++) {
-		if (test_three[j] == 1) success++;
+	for (j=0;j<=2;j++) {
+		if (test_two[j] == 1) success++;
 		if (test_one[j] == 1) success++;
-		for (i=j+1;i<=3;i++) {
-			if (test_two[j][i] == 1) {
-				success++;
-			}
-		}
 	}
-	if (success == 14) return 1;
+	if (success == 6) return 1;
 	return 0;
 }
 
 /* This computes the dimension of the automorphism group. *
  * Note that we are computing the dimension of the cone.  */
-int dim_aut(int d1, int d2, int d3, int d4)
+int dim_aut(int d1, int d2, int d3)
 {
 	int totaal;
 	totaal = 0;
-	totaal = totaal + count_sum(d1,d2,d3,d4,d1,0);
-	totaal = totaal + count_sum(d1,d2,d3,d4,d2,0);
-	totaal = totaal + count_sum(d1,d2,d3,d4,d3,0);
-	totaal = totaal + count_sum(d1,d2,d3,d4,d4,0);
+	totaal = totaal + count_sum(d1,d2,d3,d1,0);
+	totaal = totaal + count_sum(d1,d2,d3,d2,0);
+	totaal = totaal + count_sum(d1,d2,d3,d3,0);
 	return(totaal);
 }
 
 int main()
 {
-	int count,totaal,d1,d2,d3,d4,degree,pg;
+	int count,totaal,d1,d2,d3,degree,g;
 	totaal=0;
 /*
-	count = count_sum(5,6,11,27,60,1);
+	count = count_sum(5,6,11,60,1);
 	printf("Here is %d.\n",count);
 	print_sum(count);
-	d1 = well_formed(5,6,11,27);
+	d1 = well_formed(5,6,11);
 	printf("Here is %d.\n",d1);
 	d2 = d_suitable(count);
 	printf("Here is %d.\n",d2);
 	printf("%d \n ", gcd(1,2));
 	exit(0);
 */
-
-	printf("d1 d2 d3 d4 d #f dim_aut pg h11.\n");
+	printf("d1 d2 d3 d #f dim_aut g.\n");
 	d1 = 1;
 	while (d1 <= MAX) {
 	  d2 = d1;
 	  while (d2 <= MAX) {
 	    d3 = d2;
 	    while (d3 <= MAX) {
-	      d4 = d3;
-	      while ((d4 <= MAX) && (well_formed(d1, d2, d3, d4))) {
-		degree = d1+d2+d3+d4;
+		degree = d1+d2+d3;
 		while (degree <= MAX) {
-			pg = count_sum(d1,d2,d3,d4,degree-d1-d2-d3-d4,0);
-if (pg < 11) {
-			count = count_sum(d1, d2, d3, d4, degree,1);
+			g = count_sum(d1,d2,d3,degree-d1-d2-d3,0);
+if (g < 11) {
+			count = count_sum(d1, d2, d3, degree,1);
 			if (d_suitable(count)) {
-				printf("%d %d %d %d %d %d ",
-					d1,d2,d3,d4,degree,count);
-				count = dim_aut(d1,d2,d3,d4);
+				printf("%d %d %d %d %d ",
+					d1,d2,d3,degree,count);
+				count = dim_aut(d1,d2,d3);
 				printf("%d ",count);
-				printf("%d ",pg);
-				count = count_sum(
-					d1,d2,d3,d4,2*degree-d1-d2-d3-d4,0);
-				count -= count_sum(d1,d2,d3,d4,degree-d2-d3-d4,0);
-				count -= count_sum(d1,d2,d3,d4,degree-d1-d3-d4,0);
-				count -= count_sum(d1,d2,d3,d4,degree-d1-d2-d4,0);
-				count -= count_sum(d1,d2,d3,d4,degree-d1-d2-d3,0);
-				printf("%d\n",count);
+				printf("%d\n",g);
 			}
 }
 			degree++;
-		}	
-		d4++;
 	      };
 	      d3++;
 	    };
