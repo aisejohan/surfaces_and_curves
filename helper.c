5ed92c42db3e2e692234482f4237a66d524324e5
johan de jong <johan@email.com>
2008-04-20 10:21:34 -0400

    Code cleanup in helper.c
    	modified:   helper.c

--- helper.c
+++ helper.c
@@ -36,11 +36,11 @@
 
 unsigned int ivaluation(int a)
 {
-	unsigned int uit=0;
-	while(!(a % p)) {
+	unsigned int uit = 0;
+	while (!(a % p)) {
 		a = a / p;
 		uit++;
-	};
+	}
 	return(uit);
 }
 
@@ -48,21 +48,21 @@ void set_seed(unsigned int zaadje)
 {
 	int fd, uit;
 	unsigned int willekeurig;
-	if(zaadje) {
+	if (zaadje) {
 		srand(zaadje);
 		return;
-	};
-	fd = open("/dev/urandom",O_RDONLY);
-	if (fd<0) {
+	}
+	fd = open("/dev/urandom", O_RDONLY);
+	if (fd < 0) {
 		printf("Unable to open /dev/urandom. So seed=666.\n");
-		willekeurig=666;
+		willekeurig = 666;
 	} else {
-		uit=read(fd,&willekeurig,sizeof(willekeurig));
+		uit = read(fd, &willekeurig, sizeof(willekeurig));
 		if (uit <= 0) {
 			printf("Failure reading /dev/urandom. So Seed=666.\n");
-			willekeurig=666;
-		};
-	};
+			willekeurig = 666;
+		}
+	}
 	srand(willekeurig);
 	uit = close(fd);
 	return;
@@ -70,8 +70,8 @@ void set_seed(unsigned int zaadje)
 
 unsigned int count_sum(unsigned int degree)
 {
-	unsigned int count,a1,a2,a3;
-	count=0;
+	unsigned int count, a1, a2, a3;
+	count = 0;
 	for(a1=0;(d1*a1 <= degree);a1++) {
 		for(a2=0;(d1*a1+d2*a2 <= degree);a2++) {
 			for(a3=0;(d1*a1+d2*a2+d3*a3 <= degree);a3++) {
@@ -86,17 +86,17 @@ unsigned int count_sum(unsigned int degree)
 
 void print_sum(unsigned int degree)
 {
-	unsigned int a1,a2,a3;
-	for(a1=0;(d1*a1 <= degree);a1++) {
-	  for(a2=0;(d1*a1+d2*a2 <= degree);a2++) {
-	    for(a3=0;(d1*a1+d2*a2+d3*a3 <= degree);a3++) {
-	      if((degree - (a1*d1+a2*d2+a3*d3)) % d4 == 0) {
-		printf("[%d, %d, %d, %d]\n",a1,a2,a3,
-				(degree - (a1*d1+a2*d2+a3*d3))/d4);
-	      };
-	    };
-	  };
-	};
+	unsigned int a1, a2, a3;
+	for (a1 = 0; (d1*a1 <= degree); a1++) {
+	  for (a2 = 0; (d1*a1 + d2*a2 <= degree); a2++) {
+	    for (a3 = 0; (d1*a1 + d2*a2 + d3*a3 <= degree); a3++) {
+	      if ((degree - (a1*d1 + a2*d2 + a3*d3)) % d4 == 0) {
+		printf("[%d, %d, %d, %d]\n", a1, a2, a3,
+				(degree - (a1*d1 + a2*d2 + a3*d3))/d4);
+	      }
+	    }
+	  }
+	}
 	return;
 }
 
@@ -104,30 +104,30 @@ void print_sum(unsigned int degree)
  * The result may be the zero polynomial!		*/
 struct polynomial make_random(unsigned int degree, int print)
 {
-	unsigned int a1,a2,a3,a4;
+	unsigned int a1, a2, a3, a4;
 	int c;
 	struct polynomial uit;
 	struct term *uitterm;
 	struct term **ptrterm;
-	uitterm=NULL;
+	uitterm = NULL;
 	uit.degree = degree;
 	uit.leading = NULL;
 
-	if(!count_sum(degree)) {
-		printf("No monomials of degree %d! Stop.\n",degree);
+	if (!count_sum(degree)) {
+		printf("No monomials of degree %d! Stop.\n", degree);
 		exit(1);
-	};
+	}
 #ifdef INPUT_F
  #ifndef OUTPUT_LIST
 	printf("\n");
 	printf("Please input coefficients below.\n");
  #endif
 #endif
-	for(a1=0;(d1*a1 <= degree);a1++) {
-	  for(a2=0;(d1*a1+d2*a2 <= degree);a2++) {
-	    for(a3=0;(d1*a1+d2*a2+d3*a3 <= degree);a3++) {
-	      if((degree - (a1*d1+a2*d2+a3*d3)) % d4 == 0) {
-		a4 = (degree - (a1*d1+a2*d2+a3*d3))/d4;
+	for (a1 = 0; (d1*a1 <= degree); a1++) {
+	  for (a2 = 0; (d1*a1 + d2*a2 <= degree); a2++) {
+	    for (a3 = 0; (d1*a1 + d2*a2 + d3*a3 <= degree); a3++) {
+	      if ((degree - (a1*d1 + a2*d2 + a3*d3)) % d4 == 0) {
+		a4 = (degree - (a1*d1 + a2*d2 + a3*d3))/d4;
 #ifdef INPUT_F
 		/* Dummy input at first. */
 		c = 1;
@@ -136,8 +136,8 @@ struct polynomial make_random(unsigned int degree, int print)
 		c = rand() % p;
 		/* Change for compatibility with previous version.	*
 		 * This does not make any difference to the pol mod p.	*/
-		if (c < -(p-1)/2) c+=p; /* OK, this never happens. */
-		if (c > (p-1)/2) c-=p; /* This does happen. */
+		if (c < -(p - 1)/2) c += p; /* OK, this never happens. */
+		if (c > (p - 1)/2) c -= p; /* This does happen. */
 #endif
 		/* Create the new term to be put in. */
 		make_term(&uitterm);
@@ -147,32 +147,56 @@ struct polynomial make_random(unsigned int degree, int print)
 		uitterm->n4 = a4;
 		ito_sc(c,uitterm->c);
 		ptrterm = &(uit.leading);
-		while((*ptrterm) && (kleiner(uitterm, *ptrterm) == KLEINER)) {
+		while ((*ptrterm) && (kleiner(uitterm, *ptrterm) == KLEINER)) {
 			ptrterm = &((*ptrterm)->next);
-		};
+		}
 		uitterm->next = *ptrterm;
 		*ptrterm = uitterm;
 		uitterm=NULL;
-	      };
-	    };
-	  };
-	};
+	      }
+	    }
+	  }
+	}
 	uitterm = uit.leading;
 	while (uitterm) {
 		a1 = uitterm->n1;
 		a2 = uitterm->n2;
 		a3 = uitterm->n3;
 		a4 = uitterm->n4;
-		c=0;
+		c = 0;
 		printf("Coefficient of   ");
-		if(a1) {printf("x^%d",a1); c++;};
-		if((a1) && (a2+a3+a4)) {printf(" * "); c++;};
-		if(a2) {printf("y^%d",a2); c++;};
-		if((a2) && (a3+a4)) {printf(" * "); c++;};
-		if(a3) {printf("z^%d",a3); c++;};
-		if((a3) && (a4)) {printf(" * "); c++;};
-		if(a4) {printf("w^%d",a4); c++;};
-		while(8-c) {printf("   ");c++;};
+		if (a1) {
+			printf("x^%d",a1);
+			c++;
+		}
+		if ((a1) && (a2+a3+a4)) {
+			printf(" * ");
+			c++;
+		}
+		if (a2) {
+			printf("y^%d", a2);
+			c++;
+		}
+		if ((a2) && (a3 + a4)) {
+			printf(" * ");
+			c++;
+		}
+		if (a3) {
+			printf("z^%d", a3);
+			c++;
+		}
+		if ((a3) && (a4)) {
+			printf(" * ");
+			c++;
+		}
+		if (a4) {
+			printf("w^%d", a4);
+			c++;
+		}
+		while (8 - c) {
+			printf("   ");
+			c++;
+		}
 		printf("= ");
 #ifndef INPUT_F
 		if (print) printmscalar(uitterm->c);
@@ -181,9 +205,9 @@ struct polynomial make_random(unsigned int degree, int print)
  #ifdef OUTPUT_LIST
  		printf("\n");
  #else
-		scanf("%d",&c);
+		scanf("%d", &c);
  #endif
-		ito_sc(c,uitterm->c);
+		ito_sc(c, uitterm->c);
 #endif
 		uitterm = uitterm->next;
 	}
@@ -198,20 +222,20 @@ struct polynomial make_random(unsigned int degree, int print)
  * Allows f to be the zero polynomial, warns if degree not 0.	*/
 unsigned int number_terms(struct polynomial f)
 {
-	int count=0;
+	int count = 0;
 	struct term *fterm;
 
 	if(!f.leading) {
 		if(!f.degree) {
 			printf("Zero but not degree 0.");
-		};
+		}
 		return(0);
-	};
+	}
 	fterm = f.leading;
 	while(fterm) {
 		count++;
 		fterm = fterm->next;
-	};
+	}
 	return(count);
 }
 
@@ -228,16 +252,16 @@ struct polynomial frobenius(struct polynomial f)
 	uit.degree = p * f.degree;
 	ptrterm = &(uit.leading);
 	fterm = f.leading;
-	while(fterm) {
+	while (fterm) {
 		make_term(ptrterm);
-		sc_copy(fterm->c,(*ptrterm)->c);
+		sc_copy(fterm->c, (*ptrterm)->c);
 		(*ptrterm)->n1 = p*fterm->n1;
 		(*ptrterm)->n2 = p*fterm->n2;
 		(*ptrterm)->n3 = p*fterm->n3;
 		(*ptrterm)->n4 = p*fterm->n4;
 		ptrterm = &((*ptrterm)->next);
 		fterm = fterm->next;
-	};
+	}
 	return(uit);
 }
 
@@ -249,8 +273,8 @@ struct polynomial deriv(struct polynomial f, unsigned int i)
 	struct polynomial uit;
 	struct term *fterm;
 	struct term **ptrterm;
-	uit.leading=NULL;
-	ptrterm=NULL;
+	uit.leading = NULL;
+	ptrterm = NULL;
 	make_scalar(c);
 
 	fterm = f.leading;
@@ -258,83 +282,83 @@ struct polynomial deriv(struct polynomial f, unsigned int i)
 		case 1: 
 		uit.degree = (f.degree > d1) ? (f.degree - d1) : 0;
 		ptrterm = &(uit.leading);
-		while(fterm) {
-			sc_imult(fterm->n1,fterm->c,c);
-			if(!sc_is_zero(c)) {
+		while (fterm) {
+			sc_imult(fterm->n1, fterm->c,c);
+			if (!sc_is_zero(c)) {
 				make_term(ptrterm);
-				sc_copy(c,(*ptrterm)->c);
+				sc_copy(c, (*ptrterm)->c);
 				(*ptrterm)->n1 = fterm->n1 - 1;
 				(*ptrterm)->n2 = fterm->n2;
 				(*ptrterm)->n3 = fterm->n3;
 				(*ptrterm)->n4 = fterm->n4;
 				ptrterm = &((*ptrterm)->next);
-			};
+			}
 			fterm = fterm->next;
-		};
+		}
 		free_scalar(c);
 		return(uit);
 
 		case 2: 
 		uit.degree = (f.degree > d2) ? (f.degree - d2) : 0;
 		ptrterm = &(uit.leading);
-		while(fterm) {
-			sc_imult(fterm->n2,fterm->c,c);
+		while (fterm) {
+			sc_imult(fterm->n2, fterm->c, c);
 			if (!sc_is_zero(c)) {
 				make_term(ptrterm);
-				sc_copy(c,(*ptrterm)->c);
+				sc_copy(c, (*ptrterm)->c);
 				(*ptrterm)->n1 = fterm->n1;
 				(*ptrterm)->n2 = fterm->n2 - 1;
 				(*ptrterm)->n3 = fterm->n3;
 				(*ptrterm)->n4 = fterm->n4;
 				ptrterm = &((*ptrterm)->next);
-			};
+			}
 			fterm = fterm->next;
-		};
+		}
 		free_scalar(c);
 		return(uit);
 
 		case 3:
 		uit.degree = (f.degree > d3) ? (f.degree - d3) : 0;
 		ptrterm = &(uit.leading);
-		while(fterm) {
-			sc_imult(fterm->n3,fterm->c,c);
+		while (fterm) {
+			sc_imult(fterm->n3, fterm->c, c);
 			if (!sc_is_zero(c)) {
 				make_term(ptrterm);
-				sc_copy(c,(*ptrterm)->c);
+				sc_copy(c, (*ptrterm)->c);
 				(*ptrterm)->n1 = fterm->n1;
 				(*ptrterm)->n2 = fterm->n2;
 				(*ptrterm)->n3 = fterm->n3 - 1;
 				(*ptrterm)->n4 = fterm->n4;
 				ptrterm = &((*ptrterm)->next);
-			};
+			}
 			fterm = fterm->next;
-		};
+		}
 		free_scalar(c);
 		return(uit);
 		
 		case 4:
 		uit.degree = (f.degree > d4) ? (f.degree - d4) : 0;
 		ptrterm = &(uit.leading);
-		while(fterm) {
-			sc_imult(fterm->n4,fterm->c,c);
+		while (fterm) {
+			sc_imult(fterm->n4, fterm->c, c);
 			if (!sc_is_zero(c)) {
 				make_term(ptrterm);
-				sc_copy(c,(*ptrterm)->c);
+				sc_copy(c, (*ptrterm)->c);
 				(*ptrterm)->n1 = fterm->n1;
 				(*ptrterm)->n2 = fterm->n2;
 				(*ptrterm)->n3 = fterm->n3;
 				(*ptrterm)->n4 = fterm->n4 - 1;
 				ptrterm = &((*ptrterm)->next);
-			};
+			}
 			fterm = fterm->next;
-		};
+		}
 		free_scalar(c);
 		return(uit);
 		
 		default:
 		printf("Wrong again honey!");
 		exit(1);
-	};
+	}
 	exit(1);
 }
 
@@ -350,9 +374,9 @@ void rep_deriv(struct polynomial *f, unsigned int i)
 	switch (i) {
 		case 1: 
 		f->degree = (f->degree > d1) ? (f->degree - d1) : 0;
-		while(fterm) {
-			sc_imult_replace(fterm->n1,fterm->c);
-			if(!sc_is_zero(fterm->c)) {
+		while (fterm) {
+			sc_imult_replace(fterm->n1, fterm->c);
+			if (!sc_is_zero(fterm->c)) {
 				fterm->n1 = fterm->n1 - 1;
 				*ptrterm = fterm;
 				ptrterm = &(fterm->next);
@@ -361,15 +385,15 @@ void rep_deriv(struct polynomial *f, unsigned int i)
 				*ptrterm = fterm->next;
 				free_term(fterm);
 				fterm = *ptrterm;
-			};
-		};
+			}
+		}
 		return;
 
 		case 2: 
 		f->degree = (f->degree > d2) ? (f->degree - d2) : 0;
-		while(fterm) {
-			sc_imult_replace(fterm->n2,fterm->c);
-			if(!sc_is_zero(fterm->c)) {
+		while (fterm) {
+			sc_imult_replace(fterm->n2, fterm->c);
+			if (!sc_is_zero(fterm->c)) {
 				fterm->n2 = fterm->n2 - 1;
 				*ptrterm = fterm;
 				ptrterm = &(fterm->next);
@@ -378,15 +402,15 @@ void rep_deriv(struct polynomial *f, unsigned int i)
 				*ptrterm = fterm->next;
 				free_term(fterm);
 				fterm = *ptrterm;
-			};
-		};
+			}
+		}
 		return;
 
 		case 3: 
 		f->degree = (f->degree > d3) ? (f->degree - d3) : 0;
-		while(fterm) {
-			sc_imult_replace(fterm->n3,fterm->c);
-			if(!sc_is_zero(fterm->c)) {
+		while (fterm) {
+			sc_imult_replace(fterm->n3, fterm->c);
+			if (!sc_is_zero(fterm->c)) {
 				fterm->n3 = fterm->n3 - 1;
 				*ptrterm = fterm;
 				ptrterm = &(fterm->next);
@@ -395,15 +419,15 @@ void rep_deriv(struct polynomial *f, unsigned int i)
 				*ptrterm = fterm->next;
 				free_term(fterm);
 				fterm = *ptrterm;
-			};
-		};
+			}
+		}
 		return;
 
 		case 4:
 		f->degree = (f->degree > d4) ? (f->degree - d4) : 0;
-		while(fterm) {
-			sc_imult_replace(fterm->n4,fterm->c);
-			if(!sc_is_zero(fterm->c)) {
+		while (fterm) {
+			sc_imult_replace(fterm->n4, fterm->c);
+			if (!sc_is_zero(fterm->c)) {
 				fterm->n4 = fterm->n4 - 1;
 				*ptrterm = fterm;
 				ptrterm = &(fterm->next);
@@ -412,8 +436,8 @@ void rep_deriv(struct polynomial *f, unsigned int i)
 				*ptrterm = fterm->next;
 				free_term(fterm);
 				fterm = *ptrterm;
-			};
-		};
+			}
+		}
 		return;
 
 		default:
